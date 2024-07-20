60ed457bc598040ae7866fa2df6ce251c189e453
woddu <fwoddu@yahoo.com>
2024-07-20 18:00:58 +0800

    added table prop

--- resources/js/Components/CDMLMS/SingleCardCenter.jsx
+++ resources/js/Components/CDMLMS/SingleCardCenter.jsx
@@ -7,18 +7,17 @@ import React from 'react'
  * @param body Card Body
  * @returns HTML
  */
-function SingleCardCenter({ title, body, table}) {
+function SingleCardCenter({ title, body, table }) {
     return (
-
         <div className={`card mb-4`}>
             <div className={`card-body p-5`}>
                 <div className="col-xl-8 col-xxl-12">
                     <div className="text-center text-xl-start text-xxl-center mb-4 mb-xl-0 mb-xxl-4 ">
                         <h1 className="text-primary">{title}</h1>
                         <p className="text-gray-700 mb-0">{body}</p>
-                        {table}
                     </div>
                 </div>
+                {table}
             </div>
         </div>
     )
