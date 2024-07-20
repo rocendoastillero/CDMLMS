46f4fe8c2a62ce70f8d1f388f41c5c74e4d9d078
woddu <fwoddu@yahoo.com>
2024-07-20 18:01:15 +0800

    updated ui

--- resources/js/Pages/Unverified.jsx
+++ resources/js/Pages/Unverified.jsx
@@ -1,22 +1,30 @@
 import Guest from '@/Layouts/GuestLayout'
-import { Link } from '@inertiajs/react'
+import { Head, Link } from '@inertiajs/react'
 import React from 'react'
 
 export default function Unverified() {
     return (
-        <Guest>
+        <Guest
+            footer={
+                <div className='card-footer'>
+                    <div className='w-full flex flex-row justify-between items-center'>
+                        <Link className='my-3 mx-4 txt-lg text-primary' href={route('logout')} method="post">
+                            Log Out
+                        </Link>
+                        <Link className='my-3 mx-4 txt-lg text-primary' href={route('dashboard')}>
+                            Already Verified?
+                        </Link>
+                    </div>
+                </div>
+            }
+        >
+            <Head title='Unverified'/>
             <div className='px-3 my-4 text-lg font-semibold text-center'>
                 Kindly contact the Dean for the verification of your account,<br /><br />Thank you for your cooperation.
 
             </div>
-            <div className='w-full flex flex-row justify-between items-center'>
-                <Link className='my-3 mx-4 pt-4 txt-lg font-semibold' href={route('logout')} method="post" as="button">
-                    Log Out
-                </Link>
-                <Link className='my-3 mx-4 pt-4 txt-lg font-semibold' href={route('dashboard')}  as="button">
-                    Already Verified?
-                </Link>
-            </div>
+
+
         </Guest>
     )
 }
