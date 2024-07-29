7e2920603b4106b846ae57883edd6dfd214bce0d
woddu <fwoddu@yahoo.com>
2024-07-29 15:43:28 +0800

    minor changes

--- resources/js/Pages/Faculty/Announcements.jsx
+++ resources/js/Pages/Faculty/Announcements.jsx
@@ -1,13 +1,14 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Layout from "@/Layouts/Layout";
 import SingleCardWithHeader from '@/Components/CDMLMS/SingleCardWithHeader';
 import { Head } from '@inertiajs/react';
 import IconCard from '@/Components/CDMLMS/IconCard';
-import { ExclamationCircleIcon, MegaphoneIcon, PencilIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { ExclamationTriangleIcon, MegaphoneIcon } from "@heroicons/react/24/outline";
 import CardsWithSticky from '@/Components/CDMLMS/CardsWithSticky';
 import { Link } from '@inertiajs/react';
 import IconParse from '@/Components/CDMLMS/IconParse';
 import SingleCardCenter from '@/Components/CDMLMS/SingleCardCenter';
+import AlertCard from '@/Components/CDMLMS/AlertCard';
 
 
 /**
@@ -17,10 +18,6 @@ import SingleCardCenter from '@/Components/CDMLMS/SingleCardCenter';
  * @returns Page
  */
 export default function Announcements({ auth, paginated }) {
-
-    useEffect((() => {
-        console.log(paginated);
-    }), [])
 
     return (
         <Layout
@@ -36,81 +33,98 @@ export default function Announcements({ auth, paginated }) {
                 cards={
                     <>
                         {
-                            paginated.data.map(
-                                announcement => {
-                                    if (announcement.cardtype == 'center') {
-                                        return (
-                                            <SingleCardCenter
-                                                title={announcement.title}
-                                                body={announcement.content}
+                            paginated.data.length != 0 ? (
+                                paginated.data.map(
+                                    announcement => {
+                                        if (announcement.cardtype == 'center') {
+                                            return (
+                                                <SingleCardCenter
+                                                    title={announcement.title}
+                                                    body={announcement.content}
 
-                                            />
+                                                />
 
-                                        )
-                                    } else if (announcement.cardtype == 'icon') {
-                                        return (
-                                            <IconCard
-                                                icon={<IconParse icon={announcement.icon} color='text-white' />}
-                                                title={announcement.title}
-                                                body={announcement.content}
-                                                iconColor={`bg-[${announcement.color}]`}
+                                            )
+                                        } else if (announcement.cardtype == 'icon') {
+                                            return (
+                                                <IconCard
+                                                    icon={<IconParse icon={announcement.icon} color='text-white' />}
+                                                    title={announcement.title}
+                                                    body={announcement.content}
+                                                    iconColor={`bg-[${announcement.color}]`}
 
-                                            />
+                                                />
 
-                                        )
-                                    } else if (announcement.cardtype == 'header') {
-                                        return (
-                                            <SingleCardWithHeader
-                                                header={announcement.title}
-                                                body={
-                                                    <div className='mt-2'>
-                                                        {announcement.content}
-                                                    </div>
-                                                }
+                                            )
+                                        } else if (announcement.cardtype == 'header') {
+                                            return (
+                                                <SingleCardWithHeader
+                                                    header={announcement.title}
+                                                    body={
+                                                        <div className='mt-2'>
+                                                            {announcement.content}
+                                                        </div>
+                                                    }
 
-                                            />
-                                        );
+                                                />
+                                            );
+                                        }
                                     }
-                                }
+                                )
+                            ) : (
+                                <AlertCard
+                                    type='alert-info'
+                                    icon={<ExclamationTriangleIcon className="h-6 w-6" />}
+                                    title="Empty!"
+                                    message={
+                                        <div className='flex flex-row'>
+                                            No Announements
+                                        </div>
+                                    }
+                                />
                             )
                         }
                     </>
                 }
-                stickyNavHeader={`Page: ${paginated.current_page}`}
+                stickyNavHeader={paginated.data.length != 0 && (`Page: ${paginated.current_page}`)}
                 stickyNavBody={
                     <>
-                        <div className='absolute -translate-x-2/4 left-2/4 bottom-0 w-full flex flex-row items-center place-content-center'>
-                            {
-                                paginated.links.map(
-                                    (link, index) => {
-                                        if (index == 0 || index == paginated.links.length - 1) {
-                                            return (
-                                                <Link
-                                                    dangerouslySetInnerHTML={{ __html: index == 0 ? "&laquo;" : "&raquo;" }}
-                                                    className={`flex flex-row p-2 h-8 items-center place-content-center ${link.url == null && ('text-gray-500')} ${link.active ? "bg-[#044721] !border-[#044721] text-white" : ""}`}
-                                                    href={link.url}
-                                                    as='button'
-                                                    preserveScroll={true}
-                                                    disabled = {link.url == null}
-                                                />
-                                            )
+                        {
+                            paginated.data.length != 0 && (
+                                <div className='absolute -translate-x-2/4 left-2/4 bottom-1 w-full flex flex-row items-center place-content-center'>
+                                    {
+                                        paginated.links.map(
+                                            (link, index) => {
+                                                if (index == 0 || index == paginated.links.length - 1) {
+                                                    return (
+                                                        <Link
+                                                            dangerouslySetInnerHTML={{ __html: index == 0 ? "&laquo;" : "&raquo;" }}
+                                                            className={`flex flex-row p-2 h-8 items-center place-content-center ${link.url == null && ('text-gray-500')} ${link.active ? "bg-[#044721] !border-[#044721] text-white" : ""}`}
+                                                            href={link.url}
+                                                            as='button'
+                                                            preserveScroll={true}
+                                                            disabled={link.url == null}
+                                                        />
+                                                    )
 
-                                        } else {
-                                            return (
-                                                <Link
-                                                    dangerouslySetInnerHTML={{ __html: link.label }}
-                                                    className={`flex flex-row p-2 h-8 items-center place-content-center ${link.url == null && ('text-gray-500')} ${link.active ? "bg-[#044721] !border-[#044721] text-white" : ""}`}
-                                                    href={link.url}
-                                                    as='button'
-                                                    preserveScroll={true}
-                                                    disabled = {link.url == null}
-                                                />
-                                            )
-                                        }
+                                                } else {
+                                                    return (
+                                                        <Link
+                                                            dangerouslySetInnerHTML={{ __html: link.label }}
+                                                            className={`flex flex-row p-2 h-8 items-center place-content-center ${link.url == null && ('text-gray-500')} ${link.active ? "bg-[#044721] !border-[#044721] text-white" : ""}`}
+                                                            href={link.url}
+                                                            as='button'
+                                                            preserveScroll={true}
+                                                            disabled={link.url == null}
+                                                        />
+                                                    )
+                                                }
+                                            }
+                                        )
                                     }
-                                )
-                            }
-                        </div>
+                                </div>
+                            )
+                        }
                     </>
                 }
             />
